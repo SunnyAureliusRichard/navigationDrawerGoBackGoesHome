1506990db2ecfc0b6e7f29224b97cdbb198c88c0
SunnyAureliusRichard <sunnyaureliusrichard@gmail.com>
2023-05-16 16:14:54 +0100

    Added reproducible scenario described in README.md

--- App.tsx
+++ App.tsx
@@ -1,20 +1,51 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import 'react-native-gesture-handler';
+import { DrawerScreenProps, createDrawerNavigator } from '@react-navigation/drawer';
+import { Button, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+
+type Routes = {
+  Screen1: undefined
+  Screen2: undefined
+  Screen3: undefined
+};
+
+function Screen1({ navigation }: DrawerScreenProps<Routes, 'Screen1'>) {
+  return (
+    <View>
+      <Text style={{ fontSize: 30 }}>Screen 1</Text>
+      <Button title="Go back" onPress={() => navigation.goBack()} />
+    </View>
+  )
+}
+
+function Screen2({ navigation }: DrawerScreenProps<Routes, 'Screen2'>) {
+  return (
+    <View>
+      <Text style={{ fontSize: 30 }}>Screen 2</Text>
+      <Button title="Go back" onPress={() => navigation.goBack()} />
+    </View>
+  )
+}
+
+function Screen3({ navigation }: DrawerScreenProps<Routes, 'Screen3'>) {
+  return (
+    <View>
+      <Text style={{ fontSize: 30 }}>Screen 3</Text>
+      <Button title="Go back" onPress={() => navigation.goBack()} />
+    </View>
+  )
+}
+
+const Drawer = createDrawerNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Drawer.Navigator>
+        <Drawer.Screen name="Screen1" component={Screen1} />
+        <Drawer.Screen name="Screen2" component={Screen2} />
+        <Drawer.Screen name="Screen3" component={Screen3} />
+      </Drawer.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ README.md
@@ -0,0 +1,18 @@
+# navigationDrawerGoBackGoesHome
+
+This repo demonstrates an issue with the library `@react-navigation/drawer` where the `navigation.goBack` goes to the first screen registered instead of the previous screen seen.
+
+## To reproduce
+1. run `npm install`
+2. run `npm run android`
+3. go from screen 1 to screen 2
+4. go to screen 3
+5. press go back
+
+## Intended behaviour
+The user should be on screen 2.
+
+## Actual behaviour
+The user is on screen 1.
+
+The same case is reproducible by using the simulator's go back button.
